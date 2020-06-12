6463e4f8cb596c068dce6e7afd350cd520dc0222
patricknraanes <patrick.n.raanes@gmail.com>
2020-06-12 16:07:58 +0200

    Started assim.py

---
+++ assim.py
@@ -0,0 +1,14 @@
+"""DA/HM example"""
+
+## Contents:
+## ----------
+## Gen surfaces of S0
+## Investigate covariances, from one well perhaps?
+## Pick truth
+## Simulate truth/obs
+## Initial Kriging/ES
+## Assimilate w/ ES
+## Compare data mismatch and state-error
+## Assimilate w/ iES
+## Localize
+## Repeat for many experiments

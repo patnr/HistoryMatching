92eddd5aa8afc1c94749c414a7b1e39534311ecb
patricknraanes <patrick.n.raanes@gmail.com>
2020-12-03 00:53:24 +0100

    Rm colab_bootsrap in master

--- chapter1.py
+++ chapter1.py
@@ -34,9 +34,7 @@ for i, c in enumerate(name):
 # of Python in the background.
 # All of the Python code cells (in a given notebook) are connected
 # (they use the same Python kernel and thus share variables, functions, and classes).
-# Thus, the **order** in which you run the cells matters. For example:
-
-# !wget -qO- https://raw.githubusercontent.com/patricknraanes/HistoryMatching/master/colab_bootstrap.sh | bash -s
+# Thus, the **order** in which you run the cells matters.
 
 # One thing you must know is how to **restart** the Python session,
 # which clears all of your variables, functions, etc,

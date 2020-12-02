dac7000601c4c910ed26e71760f87b257dc77ccb
patricknraanes <patrick.n.raanes@gmail.com>
2020-12-03 00:52:03 +0100

    Re-instate pypi pip install of patlib

--- colab_bootstrap.sh
+++ colab_bootstrap.sh
@@ -14,14 +14,6 @@ setup () {
     URL=https://github.com/patricknraanes/HistoryMatching.git
     if [[ ! -d REPO ]]; then git clone --depth=1 $URL REPO; fi
     pip install -r REPO/requirements.txt
-
-    # Install dependency from source -- NB: no versioning!
-    # coz colab'p pip couldn't find package,
-    # even though it was on pypi (uploaded 15 min earlier).
-    # Could be that Colab/pypi needs some time to fetch NEW packages.
-    URL=https://github.com/patricknraanes/pylib.git
-    if [[ ! -d patlib ]]; then git clone --depth=1 $URL patlib; fi
-    pip install -e patlib
 }
 
 # Only run if we're on colab

503d109a7bb484a487b80b4dcf061eae58530924
patricknraanes <patrick.n.raanes@gmail.com>
2020-12-06 16:29:03 +0100

    Rm truncate_01, sigmoid. Improve RMS

--- tools.py
+++ tools.py
@@ -2,41 +2,22 @@
 
 import numpy as np
 
-
-def norm(xx):
-    # return nla.norm(xx/xx.size)
-    return np.sqrt(np.sum(xx@xx)/xx.size)
-
-
 def center(E):
     return E - E.mean(axis=0)
 
+def norm(xx):
+    # return nla.norm(xx/xx.size)
+    return np.sqrt(np.mean(xx*xx))
 
-def truncate_01(E, warn=""):
-    """Saturations should be between 0 and 1."""
-    # assert E.max() <= 1 + 1e-10
-    # assert E.min() >= 0 - 1e-10
-    if (E.max() - 1e-10 >= 1) or (E.min() + 1e-10 <= 0):
-        if warn:
-            print(f"Warning -- {warn}: needed to truncate ensemble.")
-        E = E.clip(0, 1)
-    return E
-
-
-def sigmoid(x):
-    """The logistic function."""
-    return 1/(1+np.exp(-x))
-
-
-class RMS:
+def RMS(truth, ensemble):
     """RMS error & dev."""
+    mean = ensemble.mean(axis=0)
+    err  = truth - mean
+    dev  = ensemble - mean
+    return "%6.4f (rmse),  %6.4f (std)" % (norm(err), norm(dev))
 
-    def __init__(self, truth, ensemble):
-        self.x = truth.reshape(-1)
-        self.E = ensemble.reshape((len(ensemble), -1))
-
-    def __str__(self):
-        err = self.x - self.E.mean(axis=0)
-        err = norm(err)
-        std = np.sqrt((center(self.E)**2).mean())
-        return "%6.4f (rmse),  %6.4f (std)" % (err, std)
+def RMS_all(series, vs):
+    """RMS for each item in series."""
+    for k in series:
+        if k != vs:
+            print(f"{k:12}:", RMS(series[vs], series[k]))

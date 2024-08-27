2c3368197440d3f42f1564ff9cc30dec990dfdc6
patnr <patrick.n.raanes@gmail.com>
2024-08-27 09:44:44 +0200

    Ensure jupytext hook is run by pre-commit

--- notebooks/Optimise.py
+++ notebooks/Optimise.py
@@ -1,4 +1,20 @@
 # -*- coding: utf-8 -*-
+# ---
+# jupyter:
+#   jupytext:
+#     cell_metadata_filter: -all
+#     formats: ipynb,py
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.15.1
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
+# ---
+
 # # Production optimisation with ensembles – an interactive tutorial
 #
 # Copyright Patrick N. Raanes, NORCE, 2023.
